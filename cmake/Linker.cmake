5518422976c60ba0c2f3d71ae327370a37b70e59
Template Janitor <template.janitor@example.com>
2023-10-17 09:10:53 +0000

    Cleanup template and initialize repository

--- cmake/Linker.cmake
+++ cmake/Linker.cmake
@@ -1,4 +1,4 @@
-macro(myproject_configure_linker project_name)
+macro(temp_configure_linker project_name)
   include(CheckCXXCompilerFlag)
 
   set(USER_LINKER_OPTION
--- src/sample_library/sample_library.cpp
+++ src/sample_library/sample_library.cpp
@@ -1,4 +1,4 @@
-#include <myproject/sample_library.hpp>
+#include <temp/sample_library.hpp>
 
 int factorial(int input) noexcept
 {
